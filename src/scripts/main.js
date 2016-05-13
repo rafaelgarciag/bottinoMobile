ebc79b4057d68878eadfabe3996b31e7775f7e98
rafaelgarciag <garciagallego.rafael@gmail.com>
2016-05-12 23:28:05 -0500

    Header terminado responsive, colocado aparte en archivo header.styl

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
 (function menuAppear() {
 var buttonNav = document.getElementById("buttonNav");
 var menuShow = document.getElementById("menuShow");
+var buttonNavT = document.getElementById("buttonNavT");
 
 buttonNav.addEventListener("click", onClickMenu);
+buttonNavT.addEventListener("click", onClickMenu);
 
 function onClickMenu () {
 	menuShow.classList.toggle("header-nav-list--show");
