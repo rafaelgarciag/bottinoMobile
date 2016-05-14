c01aa4bd5426e1737f9a3aa71df1982f0b03cc1d
rafaelgarciag <garciagallego.rafael@gmail.com>
2016-05-14 01:37:04 -0500

    se colocaron imágenes para las noticias, no se pudo aun concretar el menu fijo disponible con scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,23 @@
 var buttonNav = document.getElementById("buttonNav");
 var menuShow = document.getElementById("menuShow");
 var buttonNavT = document.getElementById("buttonNavT");
+var menuFixed = document.getElementById("menuFixed");
 
 buttonNav.addEventListener("click", onClickMenu);
 buttonNavT.addEventListener("click", onClickMenu);
+menuFixed.addEventListener("scroll", onScroll);
 
 function onClickMenu () {
 	menuShow.classList.toggle("header-nav-list--show");
 }
+
+//No funciona el script aún 130516
+function onScroll () {
+	if(window.scrollY > 1)
+	{
+	alert("funciona");
+	//menuFixed.classList.toggle("header-interaction-mobile--light");		
+	}
+}
+onScroll();
 }())
