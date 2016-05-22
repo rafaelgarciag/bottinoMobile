8c02156a42a8487973debd2a018440ed34dd6360
rafaelgarciag <garciagallego.rafael@gmail.com>
2016-05-22 18:56:50 -0500

    Finalización de barra de navegación desplegable responsive, falta ponerla una siguiendo a la otra

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,12 @@ var buttonNavT = document.getElementById("buttonNavT");
 var menuFixed = document.getElementById("menuFixed");
 var menuInt = document.getElementById("menuInt");
 var itemClick = document.getElementById("itemClick");
+var itemClick2 = document.getElementById("itemClick2");
 
 buttonNav.addEventListener("click", onClickMenu);
 buttonNavT.addEventListener("click", onClickMenu);
 itemClick.addEventListener("click", onMenuInt);
+itemClick2.addEventListener("click", onMenuInt2);
 menuFixed.addEventListener("scroll", onScroll);
 
 function onClickMenu () {
@@ -16,6 +18,9 @@ function onClickMenu () {
 }
 function onMenuInt () {
 	menuInt.classList.toggle("header-nav-list-item-subnav-show");
+}
+function onMenuInt2 () {
+	menuInt2.classList.toggle("header-nav-list-item-subnav2-show");
 }
 
 //No funciona el script aún 130516
