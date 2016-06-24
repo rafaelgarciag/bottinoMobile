d1d3f14e7b47639b2c38c64dc5370bed6ad699d1
rafaelgarciag <garciagallego.rafael@gmail.com>
2016-06-23 23:16:24 -0500

    Finaliza la galeria responsive para ver los productos, pendiente el ajuste para cuando se tengan las imágenes

--- src/scripts/productsDatabase.js
+++ src/scripts/productsDatabase.js
@@ -1,13 +1,10 @@
 $(function () {
-	//Ocultar la clase product luego de 5 segundos
-	setTimeout(function () {
-		$('.product').css({
-			display: 'none'
-		})
-	}, 5000)
 	//Base de datos de fotografía de productos
 	$('#032').click(function() {
 		$('.product').css({
+			display: 'block'
+		});
+		$('.gallery').css({
 			display: 'block'
 		});
 		$('#desktopView').attr('srcset', 'img/logocopain.png');
@@ -19,6 +16,9 @@ $(function () {
 	});
 	$('#030').click(function() {
 		$('.product').css({
+			display: 'block'
+		});
+		$('.gallery').css({
 			display: 'block'
 		});
 		$('#desktopView').attr('srcset', 'img/logocopain.png');
@@ -34,9 +34,69 @@ $(function () {
 			display: 'none'
 		});
 	});
-	// $("body").click(function () {
-	// 	$('.product').css({
-	// 		display: 'none'
-	// 	});
-	// });
+	//Cambiar las fotos de la galería con el click
+	$('#selector1').click(function() {
+		$('.gallery-images-item').fadeOut('slow', function() {	
+			$('#itemView1').css({
+				display: 'block'
+			});
+			$('#itemView2').css({
+				display: 'none'
+			});
+			$('#itemView3').css({
+				display: 'none'
+			});
+			$('#itemView4').css({
+				display: 'none'
+			});
+		}).fadeIn('slow');
+	});
+	$('#selector2').click(function() {
+		$('.gallery-images-item').fadeOut('slow', function() {	
+			$('#itemView1').css({
+				display: 'none'
+			});
+			$('#itemView2').css({
+				display: 'block'
+			});
+			$('#itemView3').css({
+				display: 'none'
+			});
+			$('#itemView4').css({
+				display: 'none'
+			});
+		}).fadeIn('slow');
+	});
+	$('#selector3').click(function() {
+		$('.gallery-images-item').fadeOut('slow', function() {	
+			$('#itemView1').css({
+				display: 'none'
+			})
+			$('#itemView2').css({
+				display: 'none'
+			});
+			$('#itemView3').css({
+				display: 'block'
+			});
+			$('#itemView4').css({
+				display: 'none'
+			});
+		}).fadeIn('slow');
+	});
+	$('#selector4').click(function() {
+		$('.gallery-images-item').fadeOut('slow', function() {	
+			$('#itemView1').css({
+				display: 'none'
+			});
+			$('#itemView2').css({
+				display: 'none'
+			});
+			$('#itemView3').css({
+				display: 'none'
+			});
+			$('#itemView4').css({
+				display: 'block'
+			});
+		}).fadeIn('slow');	
+	});
 }())
