df953fd410b4be8787bbed36ad4c7da4330917b7
rafaelgarciag <garciagallego.rafael@gmail.com>
2016-06-24 20:04:14 -0500

    Finaliza la creaciónd de la galería de productos responsive

--- src/scripts/productsDatabase.js
+++ src/scripts/productsDatabase.js
@@ -1,33 +1,7 @@
 $(function () {
 	//Base de datos de fotografía de productos
-	$('#032').click(function() {
-		$('.product').css({
-			display: 'block'
-		});
-		$('.gallery').css({
-			display: 'block'
-		});
-		$('#desktopView').attr('srcset', 'img/logocopain.png');
-		$('#tabletView').attr('srcset', 'img/mujer.png');
-		$('#mobileView').attr('src', 'img/carro-.png');
-		$('.product-images-item-info-material').text('some text');
-		$('.product-images-item-info-price').text('$200.000');
-		$('.product-description-paragraf').text();
-	});
-	$('#030').click(function() {
-		$('.product').css({
-			display: 'block'
-		});
-		$('.gallery').css({
-			display: 'block'
-		});
-		$('#desktopView').attr('srcset', 'img/logocopain.png');
-		$('#tabletView').attr('srcset', 'img/mujer.png');
-		$('#mobileView').attr('src', 'img/carro-.png');
-		$('.product-images-item-info-material').text('EL mejor zapato del mundo');
-		$('.product-images-item-info-price').text('$1.000.000');
-		$('.product-description-paragraf').text();
-	});
+		$('#032').click(function() {$('.product').css({display: 'block'}); $('.gallery').css({display: 'block'}); $('#desktopView').attr('srcset', 'img/mujer.png'); $('#tabletView').attr('srcset', 'img/mujer.png'); $('#mobileView').attr('src', 'img/carro-.png'); $('#desktopView2').attr('srcset', 'img/030.png'); $('#tabletView2').attr('srcset', 'img/logocopain.png'); $('#mobileView2').attr('src', 'img/logocopain.png'); $('#desktopView3').attr('srcset', 'img/026.png'); $('#tabletView3').attr('srcset', 'img/logocopain.png'); $('#mobileView3').attr('src', 'img/027.png'); $('.product-images-item-info-material').text('some text'); $('.product-images-item-info-price').text('$200.000'); $('.product-description-paragraf').text();});
+		$('#030').click(function() {$('.product').css({display: 'block'}); $('.gallery').css({display: 'block'}); $('#desktopView').attr('srcset', 'img/030.png'); $('#tabletView').attr('srcset', 'img/mujer.png'); $('#mobileView').attr('src', 'img/carro-.png'); $('#desktopView2').attr('srcset', 'img/mujer.png'); $('#tabletView2').attr('srcset', 'img/logocopain.png'); $('#mobileView2').attr('src', 'img/logocopain.png'); $('#desktopView3').attr('srcset', 'img/030.png'); $('#tabletView3').attr('srcset', 'img/logocopain.png'); $('#mobileView3').attr('src', 'img/027.png'); $('.product-images-item-info-material').text('Un zapato zapatón'); $('.product-images-item-info-price').text('$1.000.000'); $('.product-description-paragraf').text('Esta es una descripción ficty');});
 	//Ocultar la clase product al dar click en la X
 	$('#closeButton').click(function() {
 		$('.product').css({
@@ -36,7 +10,7 @@ $(function () {
 	});
 	//Cambiar las fotos de la galería con el click
 	$('#selector1').click(function() {
-		$('.gallery-images-item').fadeOut('slow', function() {	
+		$('.gallery-images-item').fadeOut('400', function() {	
 			$('#itemView1').css({
 				display: 'block'
 			});
@@ -46,13 +20,10 @@ $(function () {
 			$('#itemView3').css({
 				display: 'none'
 			});
-			$('#itemView4').css({
-				display: 'none'
-			});
-		}).fadeIn('slow');
+		}).fadeIn('400');
 	});
 	$('#selector2').click(function() {
-		$('.gallery-images-item').fadeOut('slow', function() {	
+		$('.gallery-images-item').fadeOut('400', function() {	
 			$('#itemView1').css({
 				display: 'none'
 			});
@@ -62,13 +33,10 @@ $(function () {
 			$('#itemView3').css({
 				display: 'none'
 			});
-			$('#itemView4').css({
-				display: 'none'
-			});
-		}).fadeIn('slow');
+		}).fadeIn('400');
 	});
 	$('#selector3').click(function() {
-		$('.gallery-images-item').fadeOut('slow', function() {	
+		$('.gallery-images-item').fadeOut('400', function() {	
 			$('#itemView1').css({
 				display: 'none'
 			})
@@ -78,25 +46,6 @@ $(function () {
 			$('#itemView3').css({
 				display: 'block'
 			});
-			$('#itemView4').css({
-				display: 'none'
-			});
-		}).fadeIn('slow');
-	});
-	$('#selector4').click(function() {
-		$('.gallery-images-item').fadeOut('slow', function() {	
-			$('#itemView1').css({
-				display: 'none'
-			});
-			$('#itemView2').css({
-				display: 'none'
-			});
-			$('#itemView3').css({
-				display: 'none'
-			});
-			$('#itemView4').css({
-				display: 'block'
-			});
-		}).fadeIn('slow');	
+		}).fadeIn('400');
 	});
 }())
