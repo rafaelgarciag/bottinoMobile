10dc267f33c64e46097edaa3155b06e3d4074f81
rafaelgarciag <garciagallego.rafael@gmail.com>
2017-03-20 19:51:52 -0500

    Se finaliza la revisión de todos los botones y los productos, listo para despliegue

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -123,13 +123,13 @@ $(function () {
 		/* Stuff to do when the mouse leaves the element */
 		$('#accesories-img').find('source').attr('srcset', 'img/bolsos-mobile.jpg');
 	});
-	// //<!--Start of Zendesk Chat Script-->
-	// 	window.$zopim||(function(d,s){var z=$zopim=function(c){
-	// 	z._.push(c)},$=z.s=
-	// 	d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
-	// 	_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
-	// 	$.src='https://v2.zopim.com/?4eFzrHqKImQCXkc8lOu2CeRJ3R5LI3Va';z.t=+new Date;$.
-	// 	type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-	// //<!--End of Zendesk Chat Script-->
+	//<!--Start of Zendesk Chat Script-->
+		window.$zopim||(function(d,s){var z=$zopim=function(c){
+		z._.push(c)},$=z.s=
+		d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
+		_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
+		$.src='https://v2.zopim.com/?4eFzrHqKImQCXkc8lOu2CeRJ3R5LI3Va';z.t=+new Date;$.
+		type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
+	//<!--End of Zendesk Chat Script-->
 
 }())
