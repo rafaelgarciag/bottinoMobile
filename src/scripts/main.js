b82123ecda9dc1be87ee008f1a9f4031015ab509
rafaelgarciag <garciagallego.rafael@gmail.com>
2017-06-03 17:47:27 -0500

    Se arregla tacones y botines eliminando los productos descontinuados y eliminando las redes sociales de pinterest y twitter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -93,14 +93,14 @@ $(function () {
 	$(function() {
     	$("img").lazyload({placeholder : "img/grey.gif", threshold : 100, effect : "fadeIn"});
 	});
-	//Script de Google Analytics
+	/*//Script de Google Analytics
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 			ga('create', 'UA-79744708-1', 'auto');
-			ga('send', 'pageview');
+			ga('send', 'pageview');*/
 	//Cambiar imágenes de home al hacer hover
 	$('#man-img').hover(function() {
 		/* Stuff to do when the mouse enters the element */
@@ -123,13 +123,13 @@ $(function () {
 		/* Stuff to do when the mouse leaves the element */
 		$('#accesories-img').find('source').attr('srcset', 'img/bolsos-mobile.jpg');
 	});
-	//<!--Start of Zendesk Chat Script-->
+/*	//<!--Start of Zendesk Chat Script-->
 		window.$zopim||(function(d,s){var z=$zopim=function(c){
 		z._.push(c)},$=z.s=
 		d.createElement(s),e=d.getElementsByTagName(s)[0];z.set=function(o){z.set.
 		_.push(o)};z._=[];z.set._=[];$.async=!0;$.setAttribute('charset','utf-8');
 		$.src='https://v2.zopim.com/?4eFzrHqKImQCXkc8lOu2CeRJ3R5LI3Va';z.t=+new Date;$.
 		type='text/javascript';e.parentNode.insertBefore($,e)})(document,'script');
-	//<!--End of Zendesk Chat Script-->
+	//<!--End of Zendesk Chat Script-->*/
 
 }())
